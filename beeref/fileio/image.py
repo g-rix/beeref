8a31b373bb915c370167a1e32b708c6101d6da32
g-rix <21954804+g-rix@users.noreply.github.com>
2025-10-31 09:12:19 +0200

    Update image.py
    Trying again

--- beeref/fileio/image.py
+++ beeref/fileio/image.py
@@ -93,8 +93,8 @@ def load_image(path):
     domain = '.'.join(parse.urlparse(url).netloc.split(".")[-2:])
     img = exif_rotated_image()
 
-    req = request.Request(url, headers={'User-Agent': 
-            "BeeRef Reference Image Viewer"})
+    req = request.Request(url, headers={'User-Agent':
+               "BeeRef Reference Image Viewer"})
 
     if domain == 'pinterest.com':
         try:
