076626f0ace8df8095bc5c616f7dcbcad9be9bcb
g-rix <21954804+g-rix@users.noreply.github.com>
2025-10-31 16:07:03 +0200

    Update __main__.py

--- beeref/__main__.py
+++ beeref/__main__.py
@@ -67,12 +67,11 @@ class BeeRefMainWindow(QtWidgets.QMainWindow):
         confirm = self.view.get_confirmation_unsaved_changes(
             'There are unsaved changes. Are you sure you want to quit?')
         if confirm:
-             geom = self.saveGeometry()
-             self.view.settings.setValue('MainWindow/geometry', geom)
-             event.accept()
+            geom = self.saveGeometry()
+            self.view.settings.setValue('MainWindow/geometry', geom)
+            event.accept()
         else:
-             event.ignore()
-
+            event.ignore()
 
     def __del__(self):
         del self.view
